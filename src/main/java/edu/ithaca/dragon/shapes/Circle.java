1574925331ec19c5c96a587dd47366e214ce270c
redson158 <redson@ithaca.edu>
2024-08-29 12:27:58 -0400

    implemented calcArea method

--- src/main/java/edu/ithaca/dragon/shapes/Circle.java
+++ src/main/java/edu/ithaca/dragon/shapes/Circle.java
@@ -14,7 +14,8 @@ public class Circle {
      * @return the area of this circle
      */
     public double calcArea(){
-        throw new RuntimeException("Not implemented yet");
+        double area = Math.PI * Math.pow(this.radius, 2);
+        return area;
     }
 
     /**
