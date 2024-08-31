3cad6d6465e9babbdbbf23b6e5678727747b11b2
redson158 <redson@ithaca.edu>
2024-08-31 16:17:23 -0400

    added and set up triangle and triangle test classes

---
+++ src/main/java/edu/ithaca/dragon/shapes/Triangle.java
@@ -0,0 +1,23 @@
+package edu.ithaca.dragon.shapes;
+
+public class Triangle {
+    private int base;
+    private int height;
+
+    public Triangle(int base, int height){
+        this.base = base;
+        this.height = height;
+    }
+
+    public double calcArea(){
+        
+    }
+
+    public void doubleSize(){
+
+    }
+
+    public double longestLineWithin(){
+
+    }
+}
---
+++ src/test/java/edu/ithaca/dragon/shapes/TriangleTest.java
@@ -0,0 +1,31 @@
+package edu.ithaca.dragon.shapes;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class TriangleTest {
+
+    @Test
+    public void testTriangleConstructor(){
+
+    }
+
+    @Test
+    public void testCalcArea(){
+
+    }
+
+    @Test
+    public void testDoubleSize(){
+
+    }
+
+    @Test
+    public void testLongestLineWithin(){
+        
+    }
+}
