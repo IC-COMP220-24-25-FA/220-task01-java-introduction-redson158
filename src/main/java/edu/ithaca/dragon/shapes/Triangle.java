ea0a91a813546565dee7551d38cd029a82d4174a
redson158 <redson@ithaca.edu>
2024-08-31 16:32:15 -0400

    implemented Triangle constructor

--- src/main/java/edu/ithaca/dragon/shapes/Triangle.java
+++ src/main/java/edu/ithaca/dragon/shapes/Triangle.java
@@ -1,23 +1,31 @@
 package edu.ithaca.dragon.shapes;
 
 public class Triangle {
-    private int base;
-    private int height;
+    private double base;
+    private double height;
 
-    public Triangle(int base, int height){
-        this.base = base;
-        this.height = height;
+    public Triangle(double base, double height){
+        if(base > 0){
+            if(height > 0){
+                this.base = base;
+                this.height = height;
+            }else{
+                throw new IllegalArgumentException("Height must be > 0");
+            }
+        }else{
+            throw new IllegalArgumentException("Base Must be > 0");
+        }
     }
 
     public double calcArea(){
-        
+        throw new RuntimeException("Method not implemented");
     }
 
     public void doubleSize(){
-
+        throw new RuntimeException("Method not implemented");
     }
 
     public double longestLineWithin(){
-
+        throw new RuntimeException("Method not implemented");
     }
 }
