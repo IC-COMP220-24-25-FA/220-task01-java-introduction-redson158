7db27480ac80c8566695c09197c84569802f0905
redson158 <redson@ithaca.edu>
2024-09-03 10:43:35 -0400

    added comments to largestOfThree test

--- src/test/java/edu/ithaca/dragon/practice/FunctionPracticeTest.java
+++ src/test/java/edu/ithaca/dragon/practice/FunctionPracticeTest.java
@@ -12,11 +12,15 @@ public class FunctionPracticeTest {
 
     @Test
     public void largestOfThreeTest(){
+        //middle is largest//
         assertEquals(5, FunctionPractice.largestOfThree(4, 5, 2));
+        //first is largest//
         assertEquals(5, FunctionPractice.largestOfThree(5, 4, 2));
+        //last is largest//
         assertEquals(5, FunctionPractice.largestOfThree(4, 2, 5));
+        //all are equal//
         assertEquals(5, FunctionPractice.largestOfThree(5, 5, 5));
-
+        //test error for negatives//
         assertThrows(IllegalArgumentException.class, () -> FunctionPractice.largestOfThree(-1, -1, -1));
         assertThrows(IllegalArgumentException.class, () -> FunctionPractice.largestOfThree(5, -1, 3));
         assertThrows(IllegalArgumentException.class, () -> FunctionPractice.largestOfThree(5, 3, -1));
