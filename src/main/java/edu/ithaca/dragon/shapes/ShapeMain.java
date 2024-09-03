c0d6c9739e305b8a65ba21d066558465ef0809b3
redson158 <redson@ithaca.edu>
2024-09-03 10:04:54 -0400

    tried to implement main - not sure if the double size method call is working properly

--- src/main/java/edu/ithaca/dragon/shapes/ShapeMain.java
+++ src/main/java/edu/ithaca/dragon/shapes/ShapeMain.java
@@ -1,4 +1,8 @@
 package edu.ithaca.dragon.shapes;
+import java.util.Scanner;
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ShapeMain {
     
@@ -6,5 +10,39 @@ public class ShapeMain {
         //Make a list of 5 randomly-sized rectangles and print their area and the largest line that can be drawn through them
         //Allow the user to choose one, double the size of that one, and print them all again
         //Use a loop to repeat the process 5 times
+        Scanner scanner = new Scanner(System.in);
+        Random rand = new Random();
+
+         for (int j = 0; j < 5; j++) {
+            List<Rectangle> rectList = new ArrayList<>();
+            for (int i = 0; i < 5; i++) {
+                rectList.add(new Rectangle(rand.nextDouble() * 10, rand.nextDouble() * 10));
+            }
+
+            for (int i = 0; i < rectList.size(); i++) {
+                Rectangle rectangle = rectList.get(i);
+                System.out.println("Rectangle " + (i + 1) + ":");
+                System.out.println("Area: " + rectangle.calcArea());
+                System.out.println("Longest Line Within: " + rectangle.longestLineWithin());
+            }
+
+            System.out.println("Choose a rectangle (1-5): ");
+            int choice = scanner.nextInt();
+            if (choice >= 1 && choice <= 5) {
+                rectList.get(choice - 1).doubleSize();
+            }
+
+            System.out.println("Updated Rectangles:");
+            for (Rectangle rectangle : rectList) {
+                System.out.println("Area:" + rectangle.calcArea());
+                System.out.println("Longest Line Within:" + rectangle.longestLineWithin());
+            }
+        }
+
+        scanner.close();
     }
 }
+
+    
+
+
