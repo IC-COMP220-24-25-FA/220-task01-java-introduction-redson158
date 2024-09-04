4f59c1eaa3c647b6dd2a1bc8dd0ee4a3fec9ccb2
redson158 <redson@ithaca.edu>
2024-09-04 17:27:42 -0400

    creates list of 10 randomly generated shapes and prints

--- src/main/java/edu/ithaca/dragon/shapes/ShapeMain.java
+++ src/main/java/edu/ithaca/dragon/shapes/ShapeMain.java
@@ -10,6 +10,21 @@ public class ShapeMain {
         //Make a list of 5 randomly-sized rectangles and print their area and the largest line that can be drawn through them
         //Allow the user to choose one, double the size of that one, and print them all again
         //Use a loop to repeat the process 5 times
+        Random random = new Random();
+        List<Shape> shapes = new ArrayList<Shape>();
+        for(int i = 0; i < 10; i++){
+            int shapeType = random.nextInt(3);
+            if(shapeType == 0){
+                shapes.add(new Circle(random.nextDouble() * 10));
+            }else if(shapeType == 1){
+                shapes.add(new Rectangle(random.nextDouble() * 10, random.nextDouble() * 10));
+            }else{
+                shapes.add(new Triangle(random.nextDouble() * 10, random.nextDouble() * 10));
+            }  
+        }
+        for(Shape shape : shapes){
+            System.out.println(shape.toString());
+        }
         Scanner scanner = new Scanner(System.in);
         Random rand = new Random();
         ArrayList<Rectangle> rectList = new ArrayList<>();
